2b4ff7d61c04a6fc668a1884911b60c5bdd87623
rushil1999 <shahrushil1999@gmail.com>
2023-07-19 19:44:44 -0700

    added allowed hosts

--- imageClassificationService/settings.py
+++ imageClassificationService/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zb_%(gh)un6gp0^gzn@04!w7&lyw(^$ti4=aye&)1lrzaqo)%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://fashionview-11fb61f13e9d.herokuapp.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['fashionview-11fb61f13e9d.herokuapp.com/', '127.0.0.1', 'localhost']
 
 
 # Application definition
