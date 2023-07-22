6fb5846c9ef889162c932abd399e7f90dae54e9a
rushil1999 <shahrushil1999@gmail.com>
2023-07-21 17:49:07 -0700

    added env vars

--- fashionImageClassificationService/model_loading_views.py
+++ fashionImageClassificationService/model_loading_views.py
@@ -3,14 +3,18 @@ from django.core.files.storage import FileSystemStorage
 import os
 from django.conf import settings
 from keras.models import load_model 
+import environ
 
 def load_model_from_s3(bucket_name, file_key):
     local_file_path = os.path.join(settings.BASE_DIR, 'xception_v4_large_08_0.894.h5')  # Path to store the downloaded model file locally
     file_exists = os.path.exists(local_file_path)
+    env = environ.Env()
+    environ.Env.read_env()
     if(file_exists == False):
+        s3 = boto3.client('s3',
+            aws_access_key_id=env('AWS_ACCESSKEY_ID'),
+            aws_secret_access_key=env('AWS_SECRET_ACCESS_KEY'))
         local_file_path = os.path.join(settings.BASE_DIR, 'xception_v4_large_08_0.894.h5')  # Path to store the downloaded model file locally
-        val = s3.head_object(Bucket=bucket_name, Key=file_key)
-        print("Rushil", val)
         # Download the model file from S3
         s3.download_file(bucket_name, file_key, 'xception_v4_large_08_0.894.h5' )
         fs = FileSystemStorage()
