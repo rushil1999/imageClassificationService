32d6308bb8faf9d7036d9a4d9e5f5f84135e5552
rushil1999 <shahrushil1999@gmail.com>
2023-07-31 19:17:10 -0700

    enabled CORS

--- imageClassificationService/settings.py
+++ imageClassificationService/settings.py
@@ -150,3 +150,22 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
 )
+CORS_ALLOW_METHODS = [
+       'DELETE',
+       'GET',
+       'OPTIONS',
+       'PATCH',
+       'POST',
+       'PUT',
+   ]
+CORS_ALLOW_HEADERS = [
+       'accept',
+       'accept-encoding',
+       'authorization',
+       'content-type',
+       'dnt',
+       'origin',
+       'user-agent',
+       'x-csrftoken',
+       'x-requested-with',
+   ]
